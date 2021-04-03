8011827b46af05063fea144fdb946023b81af210
Hikaru Hasegawa <drumscohika@gmail.com>
2021-04-03 11:05:13 +0900

    相談確認ビュー作成

---
+++ pages/confirm/soudan.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12" class="headline"> ご相談内容（確認） </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">項目</th>
+                <th class="text-left">回答</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in items" :key="item.name">
+                <td>{{ item.name }}</td>
+                <td>{{ item.answer }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-spacer />
+      <v-btn class="success">送信</v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { name: "制作項目", answer: "量産" },
+        { name: "使用用途", answer: "食品用" },
+      ],
+    };
+  },
+};
+</script>
