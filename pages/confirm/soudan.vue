cd6792d278dddc33087239f51cf20eb274d60814
Hikaru Hasegawa <drumscohika@gmail.com>
2021-04-16 09:35:59 +0900

    相談内容をテキストっぽく

--- pages/confirm/soudan.vue
+++ pages/confirm/soudan.vue
@@ -45,24 +45,28 @@
       <v-col cols="12" class="headline"> ご相談内容（確認） </v-col>
     </v-row>
     <v-row>
-      <v-col>
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">項目</th>
-                <th class="text-left">回答</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in items" :key="item.name">
-                <td>{{ item.name }}</td>
-                <td>{{ item.answer }}</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-col>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="4">
+            <v-subheader>制作項目</v-subheader>
+          </v-col>
+          <v-col cols="8"> 量産 </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="4">
+            <v-subheader>使用用途</v-subheader>
+          </v-col>
+          <v-col cols="8"> 食品用 </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="4">
+            <v-subheader>お問い合わせ内容</v-subheader>
+          </v-col>
+          <v-col cols="8"> とりあえずお話させてください。 </v-col>
+        </v-row>
+      </v-container>
     </v-row>
     <v-row>
       <v-spacer />
@@ -70,17 +74,3 @@
     </v-row>
   </v-container>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      items: [
-        { name: "制作項目", answer: "量産" },
-        { name: "使用用途", answer: "食品用" },
-        { name: "お問い合わせ内容", answer: "とりあえずお話させてください。" },
-      ],
-    };
-  },
-};
-</script>
