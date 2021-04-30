d04ac6c67d0a4eb177b580b68630fb75eeb6eacd
Hikaru Hasegawa <drumscohika@gmail.com>
2021-04-30 21:07:19 +0900

    不要なファイルを削除

--- build/index.js
+++
@@ -1,9 +0,0 @@
-// build/index.js
-const fs = require("fs-extra");
-const { resolve } = require("path");
-
-// Rename: _nuxt to static
-fs.moveSync(
-  resolve(__dirname, "../dist/_nuxt"),
-  resolve(__dirname, "../dist/static")
-);
